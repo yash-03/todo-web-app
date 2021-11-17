665e37bae9e64940281840bff6aecf752d506f67
yash-03 <yksrivastav1@gmail.com>
2021-11-17 22:58:25 +0530

    #WIP - todo list home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Button from "./components/Button";
+import Filter from "./components/Filter";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Todo List";
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Todo List</h2>
+        <div className="todo-list-controller">
+          <Button text="+" />
+          <Filter />
+        </div>
       </header>
     </div>
   );

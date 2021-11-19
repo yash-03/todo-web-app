588da81c23d21fabd8974ca381918ed47240fa9f
yash-03 <yksrivastav1@gmail.com>
2021-11-19 21:57:36 +0530

    #WIP - seperate raw data

--- src/App.js
+++ src/App.js
@@ -3,34 +3,10 @@ import Button from "./components/Button";
 import Filter from "./components/Filter";
 import Table from "./components/Table/Table";
 import Action from "./components/Action";
+import { statusList, record } from "./constants.js";
 import "./App.css";
 
 function App() {
-  const statusList = [
-    { id: 1, title: "Pending", color: "orange" },
-    { id: 2, title: "Todo", color: "yellow" },
-    { id: 3, title: "Delete", color: "red" },
-    { id: 4, title: "Working", color: "green" },
-  ];
-
-  const record = [
-    {
-      id: 1,
-      task: "first",
-      status: 1,
-    },
-    {
-      id: 2,
-      task: "second",
-      status: 3,
-    },
-    {
-      id: 3,
-      task: "third",
-      status: 4,
-    },
-  ];
-
   const todoTaskList = () => {
     return record.map((task) => {
       const { title, color } = statusList.find((el) => el.id === task.status);

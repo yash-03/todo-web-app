d2418efb77735f27e4979ac00cf8815381b5d3b8
yash-03 <yksrivastav1@gmail.com>
2021-11-27 17:22:44 +0530

    form set

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Modal from "./common/modal/Modal";
 import TextField from "./common/TextField";
 import TextArea from "./common/TextArea";
@@ -7,18 +8,30 @@ import Select from "./common/Select";
 import { priorities, status } from "../constants";
 import "./EditForm.css";
 
-function EditForm() {
+function EditForm({ open, handleClose }) {
   const submitHandler = () => {
     console.log("test submit");
   };
   return (
-    <Modal>
+    <Modal open={open} handleClose={handleClose}>
       <div>
         <form onSubmit={submitHandler} className="form">
           <h2>New Task</h2>
           <TextField name="task" label="Task Name" />
-          <Select options={priorities} label="Priority" />
-          <Select options={status} label="status" />
+          <Select
+            options={priorities}
+            name="priority"
+            value=""
+            label="Priority"
+            changeHandler={() => "test"}
+          />
+          <Select
+            options={status}
+            name="status"
+            value=""
+            label="status"
+            changeHandler={() => "test"}
+          />
           <TextArea name="description" label="Description" />
           <Button text="Save" classes="form-button" />
         </form>
@@ -26,5 +39,10 @@ function EditForm() {
     </Modal>
   );
 }
+
+EditForm.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
 
 export default EditForm;
